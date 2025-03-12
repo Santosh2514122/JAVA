059ff3fa6575491fc33fe1186b292d8c398da9ae
Santosh Yadav <santoshgkp107@gmail.com>
2025-03-12 23:56:46 +0530

    sk003

---
+++ BASIC INPUT_OUTPUT/example.java
@@ -0,0 +1,34 @@
+
+     public class example 
+     { 
+     static void fun1()// function 
+     {
+        System.out.println("Santosh Yadav");
+     }
+     static void  fun2()
+     {
+        System.out.println(" bittu kushawaha ");
+     }
+     static void  fun3()
+     {
+        System.out.println(" bittu kushawaha ");
+     }
+      public static void main(String[] args) 
+      {
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        //System.out.println("Sntosh yadav ");
+        fun1();
+        fun2();
+        fun3();
+    } 
+}
+/*
+A
+B
+C
+Santosh Yadav
+ bittu kushawaha 
+ bittu kushawaha 
+ */
