75e4b1478cc95e6fe3705807f8e9d9f006ad8e4a
Santosh Yadav <santoshgkp107@gmail.com>
2025-03-12 23:31:44 +0530

    Add files via upload

---
+++ Basicinnput.java
@@ -0,0 +1,16 @@
+public class Basicinnput {
+    public  static  void main ()
+    {
+        int a=6;
+        int b=4;
+        int c=4;
+        int sum=(a+b+c);
+        int sub=(a+b-c);
+        System.out.println(sum);
+        System.out.println(sub);
+
+        
+
+
+    }
+}
