496000432eae5ef29972b2ef12672b2d83482a6e
Santosh Yadav <santoshgkp107@gmail.com>
2025-03-12 23:36:17 +0530

    sk003

---
+++ dataType.java
@@ -0,0 +1,48 @@
+/* 
+ 1-integer 
+ byte 1byte = 8bit 2^8 range(2)
+ short  2 byte =16 bit range (2^15-1  2^15)
+ int    4 byte = 32 bit range  ( 2^31-1  2^31)
+ long    8 byte  = 64 bit range  ( 2^63-1 )
+    float
+    double
+    char
+     boolean
+*/
+
+public   class dataType {
+   public  static  void main(String[]args)
+{
+  
+   System.out.print(" santosh yadav \n");
+   System.out.println(" santosh yadav ");
+   System.out.print(" santosh yadav\nbittu\najay\n");
+  System.err.println(" print the pattern ");
+   System.out.println("*");
+   System.out.println("**");
+   System.out.println("***");
+   System.out.println("****");
+   System.out.println("*****");
+
+
+   
+   
+
+}
+   
+}
+/*
+//output 
+ santosh yadav 
+ santosh yadav 
+ santosh yadav
+bittu
+ajay
+PS C:\Users\santo\OneDrive\Desktop\coding> 
+ 
+public class dataType{
+   public static void main(String[]args){
+      //variable
+   }
+}
+*/
