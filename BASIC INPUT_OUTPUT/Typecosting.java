372604fcc4a156137b289bfcfefcc9448309d598
Santosh Yadav <santoshgkp107@gmail.com>
2025-03-13 10:19:19 +0530

    Add files via upload

---
+++ BASIC INPUT_OUTPUT/Typecosting.java
@@ -0,0 +1,21 @@
+ //Typecosting
+//1:-widening cast(atomattically)-converge smaller to a large data type
+//byte -> shote->char->int->float->double
+//narrowing type casting  or explicit(manually)-converge large data into small data type
+//double->float ->long ->int->char->short->byte
+public class Typecosting
+ {
+public static void main(String[]arg){
+  /* 
+    byte x=12;
+  float y;
+  y=x;  
+ System.out.println("y");
+*/
+//explicit
+double a=222.342;
+int info;
+info=(int)a;
+System.out.println(info);
+}    
+}
