abea30291552cc5791424a999b9f3bab1d4d9ce5
Santosh Yadav <santoshgkp107@gmail.com>
2025-03-13 10:18:02 +0530

    Add files via upload

---
+++ BASIC INPUT_OUTPUT/Scapesequence.java
@@ -0,0 +1,38 @@
+
+
+public class Scapesequence {
+  public static void main(String[] args) {
+      System.out.println("santosh\nBittu\najay\namit\nusha yadav\numesh yadav");
+      //change for line
+      System.out.println("santosh\tbittu\tajay\tusha");
+      //for tab space []
+   System.out.println("Santosh\'bittu\'");
+   //for single quote
+   System.out.println("Santosh\"bittu\"");
+ //for double quote
+   System.out.println("Santosh\\bittu\\");
+   //for message whithin \
+   System.out.println("Santosh\bbittu");
+   // inserts backspace
+   System.out.println("Santosh\fbittu");
+   //forate feed
+   System.out.println("Santosh\rbittu");
+  } 
+  } 
+/*
+ santosh
+Bittu
+ajay
+amit
+usha yadav
+umesh yadav
+santosh bittu   ajay    usha
+Santosh'bittu'
+Santosh"bittu"
+Santosh\bittu\
+Santosbittu
+Santosh
+bittu
+bittush
+ */
+
