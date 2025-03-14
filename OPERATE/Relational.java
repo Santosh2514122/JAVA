61f630fe3ba86a5d1c46e258d66cced48d1c727b
Santosh Yadav <santoshgkp107@gmail.com>
2025-03-14 10:32:00 +0530

    Add files via upload

---
+++ OPERATE/Relational.java
@@ -0,0 +1,17 @@
+//==,!=;>,>=,<,<=
+public class Relational {
+    public static void main(String[] args) {
+        int a=4;
+        int b=6;
+        System.out.println("a>b is"+(a>b));//f
+        System.out.println("a>=b is"+(a>=b));//f
+        System.out.println("a<b is"+(a<b));//t
+        System.out.println("a<=b is"+(a<=b));//t
+        System.out.println("a<=b is"+(a<=b));//t
+        System.out.println("a==b is"+(a==b));//f
+        System.out.println("a!=b is"+(a!=b));//t
+    }
+    }
+
+    
+
---
+++ OPERATE/Uoperator.java
@@ -0,0 +1,19 @@
+//++,--;
+import java.util.Scanner;
+public class Uoperator {
+    public static void main(String[] args)
+    {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter the number:");
+       int a=sc.nextInt();
+       System.out.println("enter the second number:");
+       int b=sc.nextInt();
+       sc.close();
+       System.out.println("postincrement:"+(a++));
+       System.out.println("preincrement:"+(++a));
+       System.out.println("postincrement:"+(b--));
+       System.out.println("preincrement:"+(--b));
+    
+
+    }
+}
