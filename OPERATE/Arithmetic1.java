02e6477771a8c0afcaf7628adc8d97d33291db9e
Santosh Yadav <santoshgkp107@gmail.com>
2025-03-14 09:27:01 +0530

    Add files via upload

---
+++ OPERATE/Arithmetic1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Arithmetic1 {
+    public static void main(String[]args)
+    {  Scanner obj =new Scanner(System.in);
+      System.out.println("enter the first number:");
+      int a=obj.nextInt();
+      System.out.println("enter the second Number :");
+      int b=obj.nextInt();
+      int sum=a+b;
+      int mul=a*b;
+      int div=a/b;
+      int modul=a%b;
+      System.out.println("sum="+sum);
+      System.out.println("mul="+mul);
+      System.out.println("div="+div);
+      System.out.println("modul="+modul);
+    }
+    }
+
