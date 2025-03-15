0214e6d0b5f5bceef8bbe4b34fd9932291fa0a3a
Santosh Yadav <santoshgkp107@gmail.com>
2025-03-15 10:13:43 +0530

    Add files via upload

---
+++ BitwiseOperater.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class BitwiseOperater {
+  public static void main(String[]args){
+    Scanner obj=new Scanner(System.in);
+    System.out.println("enter the number:");
+    int a =obj.nextInt();
+    System.out.println("enter the second number:");
+        int b =obj.nextInt();
+        System.out.println("Bitwise AND"+(a&b));
+        System.out.println("Bitwise OR"+(a|b));
+        System.out.println("Bitwise XOR"+(a^b));
+        System.out.println("Bitwise XOR"+(~a));
+    
+  }
+}
