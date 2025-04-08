ba562ad1ee1f8660c5beefd115727a4db2c1c7a8
Santosh Yadav <santoshgkp107@gmail.com>
2025-04-08 22:44:55 +0530

    Update Swap.java

--- 1BASIC INPUT_OUTPUT/Swap.java
+++ 1BASIC INPUT_OUTPUT/Swap.java
@@ -2,7 +2,7 @@ public class Swap {
     public static void main(String[]args){
      int a=4,b=5;
      System.out.println("before the swap ");
-     System.out.println("a= " +a+  "\tb=" +b);
+     System.out.println("a= "+a+"\tb="+b);
      /* 
      int temp;
      temp=a;
