44cfdbbd7559428e1160e521718c03861bcfb499
Santosh Yadav <santoshgkp107@gmail.com>
2025-04-24 23:09:06 +0530

    Add files via upload

---
+++ 1BASIC INPUT_OUTPUT/sumOfTwoNumber.java
@@ -0,0 +1,12 @@
+package basicprogram;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class sumOfTwoNumber {
+
+    public  static void main(String[]args)
+    {   int a=5,b=6;
+        System.out.println("sum of two number:"+(a+b));
+    }
+
+}
