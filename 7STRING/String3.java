2dd990ac945ac958e08ae9685033ab9e11342680
Santosh Yadav <santoshgkp107@gmail.com>
2025-04-29 23:29:14 +0530

    Update String3.java

--- 7STRING/String3.java
+++ 7STRING/String3.java
@@ -2,11 +2,9 @@ package String;
 
 public class String3 {
     public static void main(String args[])
-    {
-        // Creating two string literals
+    {// Creating two string literals
         String s1 = "Bittu";
         String s2 = "Santosh";
-
         // check s2 equal to s1 are not
         System.out.println(s1.equals(s2));
 
