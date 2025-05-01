e7df5e945ecbbe19b4e593d4e389ad487fa7fe45
Santosh Yadav <santoshgkp107@gmail.com>
2025-05-01 21:57:05 +0530

    Update String4.java

--- 7STRING/String4.java
+++ 7STRING/String4.java
@@ -3,11 +3,8 @@ package String;
 public class String4 {
     public static void main(String args[])
     {
-        // Creating two string literals
         String s1 = "SantoshYadav";
         String s2 = "Santosh";
-
-        // checks2 is present in s1 or not
         System.out.println(s1.contains(s2));
     }
 }
