72fc259bfdba041e1a4ddb6d1e49cf08bd2abe04
Santosh Yadav <santoshgkp107@gmail.com>
2025-05-30 23:32:57 +0530

    Update Arithmetic1.java

--- 3OPERATE/Arithmetic1.java
+++ 3OPERATE/Arithmetic1.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class Arithmetic1 {
+public class Arithmetic1
+    {
     public static void main(String[]args)
     {  Scanner obj =new Scanner(System.in);
       System.out.println("enter the first number:");
